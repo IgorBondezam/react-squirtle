003c0bb4db1a7c00673314c50f2ca2a552b7320b
Igor Bondezam França <igorbfraca@hotmail.com>
2024-06-14 23:41:24 -0300

    Correção aspas out

--- src/components/CardSection/card-section-component.tsx
+++ src/components/CardSection/card-section-component.tsx
@@ -1,15 +1,17 @@
 import card from './card-section.module.css'
 
-interface CardProp{
-  title: string;
-  children: React.ReactNode;
+interface CardProp {
+    title: string;
+    children: React.ReactNode;
 }
 
 export default function CardSection(props: CardProp) {
-  return (
-    <section className={card.card}>
-      <h2>{props.title}</h2>
-      <p className={card.cardTitle}>{props.children}</p>
-    </section>
-  );
+    return (
+        <>
+            <section className={card.card}>
+                <h2>{props.title}</h2>
+                <p className={card.cardTitle}>{props.children}</p>
+            </section>
+        </>
+    );
 }
