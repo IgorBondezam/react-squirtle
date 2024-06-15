d4b34ac658fa188dadf612ca6917a5237b5a834f
Igor Bondezam França <igorbfraca@hotmail.com>
2024-06-15 00:09:09 -0300

    Correção arquivo index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import PaginaSquirtle from "@/components/PaginaSquirtle/paginaSquirtle";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Squirtle - React",
-  description: "Site do Squirtle em React",
+  title: "Squirtle-app",
+  description: "React squirtle app",
 };
 
-export default function RootLayout() {
+export default function RootLayout({
+                                     children,
+                                   }: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="pt-br">
-      <body>{<PaginaSquirtle/>}</body>
-    </html>
+      <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+      </html>
   );
 }
